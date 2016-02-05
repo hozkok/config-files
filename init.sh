eeaf6621c997e80fd2e086f43e763014e6f0ec91
hakan ozkok <hakanozkok@gmail.com>
2016-02-05 15:24:24 +0000

    no comment

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
-#!/bin/bash
+for dotfile in *.dotfile; do
+    base_folder=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+    link_name=.${dotfile%.dotfile}
+    ln -s $base_folder/$dotfile $HOME/$link_name
+done
