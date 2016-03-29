0b44a902d3913b26186d3505b55d96f0674e072f
hakan ozkok <hakanozkok@gmail.com>
2016-03-29 18:59:36 +0100

    zshrc added, bug fix on init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 for dotfile in *.dotfile; do
-    base_folder=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+    base_folder=$(dirname $(readlink -f $0))
     link_name=.${dotfile%.dotfile}
     ln -s $base_folder/$dotfile $HOME/$link_name
 done
