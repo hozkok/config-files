9f6dd8ddb4f0c2462c546a664082ca3542b23583
hozkok <hozkok@LAPTOP-7PIDK1VL>
2023-04-17 00:00:37 +0100

    working solution without coc

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require('plugins')
 vim.o.foldmethod = 'indent'
 vim.o.foldlevel = 99
 vim.opt.diffopt = vim.opt.diffopt + 'vertical'
@@ -44,7 +45,7 @@ vim.o.wrapmargin = 0
 vim.o.backspace = 'indent,eol,start'
 vim.o.autoindent = true
 vim.keymap.set('n', '<leader>b', ':ls<CR>:b<Space>', {remap = false})
-os.execute('!mkdir -p ~/.vim/backup ~/.vim/swap ~/.vim/undo')
+os.execute('mkdir -p ~/.vim/backup ~/.vim/swap ~/.vim/undo')
 vim.o.backupdir = '~/.vim/backup//'
 vim.o.directory = '~/.vim/swap//'
 vim.o.undodir = '~/.vim/undo//'
@@ -84,104 +85,4 @@ if vim.fn.executable('clip.exe') then
     },
     cache_enabled = 0,
   }
-end
-
------------------
-require("nvim-tree").setup()
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "python" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
--- neovim-dap and neotest configs for debugging
-do
-  local dap = require('dap')
-  local dappy = require('dap-python')
-  local widgets = require('dap.ui.widgets')
-  local neotest = require('neotest')
-  local neotestpy = require('neotest-python')
-  dappy.setup('~/.debugpy/bin/python')
-  dappy.test_runner = 'pytest'
-  vim.keymap.set('n', '<leader>x', function() end) -- noop to prevent `x` on timeout
-  vim.keymap.set('n', '<leader>xc', function() dap.continue() end)
-  vim.keymap.set('n', '<leader>xn', function() dap.step_over() end)
-  vim.keymap.set('n', '<leader>xs', function() dap.step_into() end)
-  vim.keymap.set('n', '<leader>xr', function() dap.step_out() end)
-  vim.keymap.set('n', '<leader>xu', function() dap.up() end)
-  vim.keymap.set('n', '<leader>xd', function() dap.down() end)
-  vim.keymap.set('n', '<leader>xb', function() dap.toggle_breakpoint() end)
-  vim.keymap.set('n', '<leader>xl', function() dap.run_to_cursor() end)
-  vim.keymap.set('n', '<leader>xii', function() dap.repl.open() end)
-  vim.keymap.set({'n', 'v'}, '<Leader>xh', function()
-    widgets.hover()
-  end)
-  vim.keymap.set({'n', 'v'}, '<Leader>xp', function()
-    widgets.preview()
-  end)
-  vim.keymap.set('n', '<Leader>xff', function()
-    widgets.centered_float(widgets.frames)
-  end)
-  vim.keymap.set('n', '<Leader>xfs', function()
-    widgets.centered_float(widgets.scopes)
-  end)
-  neotest.setup({
-    adapters = {
-      neotestpy({
-        dap = { justMyCode = false },
-      })
-    },
-    status = {
-      enabled = false,
-    },
-  })
-  vim.keymap.set('n', '<leader>xtt', function() neotest.run.run() end)
-  vim.keymap.set('n', '<leader>xtd', function()
-    neotest.run.run({strategy='dap'})
-  end)
-  vim.api.nvim_create_user_command(
-    'DapClearBreakpoints', dap.clear_breakpoints, {nargs=0}
-  )
-  vim.api.nvim_create_user_command(
-    'NeotestOutput', neotest.output_panel.toggle, {nargs=0}
-  )
-  vim.api.nvim_create_user_command(
-    'NeotestSummary', neotest.summary.toggle, {nargs=0}
-  )
 end
--- plugins.lua
+++ plugins.lua
@@ -28,7 +28,66 @@ return require('packer').startup(function (use)
   }
 
   -- debugging
-  use 'mfussenegger/nvim-dap'
+  use {
+    'mfussenegger/nvim-dap',
+    'mfussenegger/nvim-dap-python',
+    'nvim-neotest/neotest',
+    'nvim-neotest/neotest-python',
+    config = function()
+      local dap = require('dap')
+      local dappy = require('dap-python')
+      local widgets = require('dap.ui.widgets')
+      local neotest = require('neotest')
+      local neotestpy = require('neotest-python')
+      dappy.setup('~/.debugpy/bin/python')
+      dappy.test_runner = 'pytest'
+      vim.keymap.set('n', '<leader>x', function() end) -- noop to prevent `x` on timeout
+      vim.keymap.set('n', '<leader>xc', function() dap.continue() end)
+      vim.keymap.set('n', '<leader>xn', function() dap.step_over() end)
+      vim.keymap.set('n', '<leader>xs', function() dap.step_into() end)
+      vim.keymap.set('n', '<leader>xr', function() dap.step_out() end)
+      vim.keymap.set('n', '<leader>xu', function() dap.up() end)
+      vim.keymap.set('n', '<leader>xd', function() dap.down() end)
+      vim.keymap.set('n', '<leader>xb', function() dap.toggle_breakpoint() end)
+      vim.keymap.set('n', '<leader>xl', function() dap.run_to_cursor() end)
+      vim.keymap.set('n', '<leader>xii', function() dap.repl.open() end)
+      vim.keymap.set({'n', 'v'}, '<Leader>xh', function()
+        widgets.hover()
+      end)
+      vim.keymap.set({'n', 'v'}, '<Leader>xp', function()
+        widgets.preview()
+      end)
+      vim.keymap.set('n', '<Leader>xff', function()
+        widgets.centered_float(widgets.frames)
+      end)
+      vim.keymap.set('n', '<Leader>xfs', function()
+        widgets.centered_float(widgets.scopes)
+      end)
+      neotest.setup({
+        adapters = {
+          neotestpy({
+            dap = { justMyCode = false },
+          })
+        },
+        status = {
+          enabled = false,
+        },
+      })
+      vim.keymap.set('n', '<leader>xtt', function() neotest.run.run() end)
+      vim.keymap.set('n', '<leader>xtd', function()
+        neotest.run.run({strategy='dap'})
+      end)
+      vim.api.nvim_create_user_command(
+        'DapClearBreakpoints', dap.clear_breakpoints, {nargs=0}
+      )
+      vim.api.nvim_create_user_command(
+        'NeotestOutput', neotest.output_panel.toggle, {nargs=0}
+      )
+      vim.api.nvim_create_user_command(
+        'NeotestSummary', neotest.summary.toggle, {nargs=0}
+      )
+    end
+  }
   use {
     'mfussenegger/nvim-dap-python',
     run = 'python -m venv ~/.debugpy && ~/.debugpy/bin/python -m pip install debugpy'
@@ -51,7 +110,6 @@ return require('packer').startup(function (use)
 
   -- universal vim settings
   use 'tpope/vim-sensible'
-  use 'tpope/vim-surround'
 
   -- personal note taking plugin (similar to orgmode in emacs)
   use {
@@ -69,10 +127,57 @@ return require('packer').startup(function (use)
     'nvim-tree/nvim-tree.lua',
     requires = {'nvim-tree/nvim-web-devicons'},
     config = function()
-      vim.keymap.set('<C-n>', ':NvimTreeToggle<CR>')
+      require('nvim-tree').setup()
+      vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
     end,
   }
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        ensure_installed = { "lua", "vim", "vimdoc", "query", "python" },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = false,
+
+        -- List of parsers to ignore installing (for "all")
+        ignore_install = { "javascript" },
+
+        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+        highlight = {
+          enable = true,
+
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- list of language that will be disabled
+          -- disable = { "c", "rust" },
+          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+              if ok and stats and stats.size > max_filesize then
+                  return true
+              end
+          end,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end
+  }
   use 'vim-airline/vim-airline'
 
   -- smooth scrolling
@@ -101,30 +206,30 @@ return require('packer').startup(function (use)
       end
       keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
       -- Use <c-space> to trigger completion
-      keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
-      vim.api.nvim_create_augroup("CocGroup", {})
-      vim.api.nvim_create_autocmd("CursorHold", {
-        group = "CocGroup",
-        command = "silent call CocActionAsync('highlight')",
-        desc = "Highlight symbol under cursor on CursorHold"
-      })
-      local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-      keyset("n", "<leader>d", "<Plug>(coc-definition)", {silent = true})
-      keyset("n", "<leader>gy", "<Plug>(coc-type-definition)", {silent = true})
-      keyset("n", "<leader>gi", "<Plug>(coc-implementation)", {silent = true})
-      keyset("n", "<leader>gr", "<Plug>(coc-references)", {silent = true})
-      keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
-      -- scroll popup
-      keyset("n", "<C-j>", 'coc#float#has_scroll() ? coc#float#scroll(1, 5)" : "<c-w>j"',
-             {silent = true, nowait = true, expr = true})
-      keyset("n", "<C-k>", 'coc#float#has_scroll() ? coc#float#scroll(0, 5)" : "<c-w>k"',
-             {silent = true, nowait = true, expr = true})
-      keyset("i", "<C-j>", 'coc#float#has_scroll() ? "\\<c-j>=coc#float#scroll(1, 5)\\<cr>" : "\\<Right>"',
-             {silent = true, nowait = true, expr = true, noremap=true})
-      keyset("i", "<C-k>", 'coc#float#has_scroll() ? "\\<c-j>=coc#float#scroll(1, 5)\\<cr>" : "\\<Left>"',
-             {silent = true, nowait = true, expr = true, noremap=true})
-      -- suggestion select
-      keyset("i", '<cr> coc#pum#visible() ? coc#pum#confirm() : "\\<CR>"', {expr=true, noremap=true})
+      -- keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
+      -- vim.api.nvim_create_augroup("CocGroup", {})
+      -- vim.api.nvim_create_autocmd("CursorHold", {
+      --   group = "CocGroup",
+      --   command = "silent call CocActionAsync('highlight')",
+      --   desc = "Highlight symbol under cursor on CursorHold"
+      -- })
+      -- local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+      -- keyset("n", "<leader>d", "<Plug>(coc-definition)", {silent = true})
+      -- keyset("n", "<leader>gy", "<Plug>(coc-type-definition)", {silent = true})
+      -- keyset("n", "<leader>gi", "<Plug>(coc-implementation)", {silent = true})
+      -- keyset("n", "<leader>gr", "<Plug>(coc-references)", {silent = true})
+      -- keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
+      -- -- scroll popup
+      -- keyset("n", "<C-j>", 'coc#float#has_scroll() ? coc#float#scroll(1, 5)" : "<c-w>j"',
+      --        {silent = true, nowait = true, expr = true})
+      -- keyset("n", "<C-k>", 'coc#float#has_scroll() ? coc#float#scroll(0, 5)" : "<c-w>k"',
+      --        {silent = true, nowait = true, expr = true})
+      -- keyset("i", "<C-j>", 'coc#float#has_scroll() ? "\\<c-j>=coc#float#scroll(1, 5)\\<cr>" : "\\<Right>"',
+      --        {silent = true, nowait = true, expr = true, noremap=true})
+      -- keyset("i", "<C-k>", 'coc#float#has_scroll() ? "\\<c-j>=coc#float#scroll(1, 5)\\<cr>" : "\\<Left>"',
+      --        {silent = true, nowait = true, expr = true, noremap=true})
+      -- -- suggestion select
+      -- keyset("i", '<cr> coc#pum#visible() ? coc#pum#confirm() : "\\<CR>"', {expr=true, noremap=true})
     end
   }
 
@@ -142,7 +247,7 @@ return require('packer').startup(function (use)
 
   use 'godlygeek/tabular'
 
-  use 'plasticbok/vim-markdown'
+  use 'plasticboy/vim-markdown'
 
   use {
     'scrooloose/nerdcommenter',
