1fc8948125e213fb9d9aa88cf0572c40afdcd349
hozkok <hozkok@LAPTOP-7PIDK1VL>
2023-04-20 00:01:57 +0100

    treesitter indent enabled

--- plugins.lua
+++ plugins.lua
@@ -207,6 +207,9 @@ return require('packer').startup(function (use)
           -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
         },
+        indent = {
+          enable = true,
+        }
       }
     end
   }
